b04ccf05396280164f89733802483b3af26dd015
Elena <e.n.baskaeva@gmail.com>
2020-04-14 16:50:09 +0700

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 15_000;
+        int bonusmiles = ticketprice / 20;
+        System.out.println(bonusmiles);
+    }
+}
