ef9ab98d7fb46c8c08d006adb3c824460b5f4472
Elena <e.n.baskaeva@gmail.com>
2020-04-14 18:47:00 +0700

    fix main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 15_000;
-        int bonusmiles = ticketprice / 20;
-        System.out.println(bonusmiles);
+        int ticketPrice = 15_000;
+        int bonusMiles = ticketPrice / 20;
+        System.out.println(bonusMiles);
     }
 }
